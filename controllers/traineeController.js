56ab96e33c9bd85f97b5a0d9b873a128921cb93a
MD Milon <milon@kali>
2025-01-19 20:50:24 +0600

    update profile logic

--- controllers/traineeController.js
+++ controllers/traineeController.js
@@ -1,8 +1,66 @@
+const Users = require("../models/users");
+const mongoose = require("mongoose");
 const updateProfile = async (req, res) => {
-  res.send(req.body);
+  const { gender, number, age, location } = req.body;
+  if (gender?.trim() && number?.trim() && age?.trim() && location?.trim()) {
+    try {
+      const user = await Users.findById(req.userInfo._id);
+      if (!user)
+        return res.send({
+          success: false,
+          statusCode: 400,
+          message: "User not found",
+        });
+
+      user.profile.gender = gender;
+      user.profile.number = parseInt(number);
+      user.profile.age = parseInt(age);
+      user.profile.location = location;
+      await user.save();
+      return res.send({
+        success: true,
+        statusCode: 200,
+        message: "Profile success to update",
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.send({
+        success: false,
+        statusCode: 400,
+        message: error.message,
+      });
+    }
+  } else {
+    return res.send({
+      success: false,
+      statusCode: 400,
+      message: "All feild are required",
+    });
+  }
 };
 const profile = async (req, res) => {
-  res.send(req.userInfo);
+  try {
+    const user = await Users.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.userInfo._id),
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          email: 1,
+          profile: 1,
+          createAt: 1,
+        },
+      },
+    ]);
+
+    res.send(user[0] || {});
+  } catch (error) {
+    console.log("error");
+    res.send(error);
+  }
 };
 
 module.exports = { updateProfile, profile };
--- routes/traineeRoutes.js
+++ routes/traineeRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const verifyUser = require("../middlewares/verifyUser");
-const { profile } = require("../controllers/traineeController");
+const { profile, updateProfile } = require("../controllers/traineeController");
 
 const route = express.Router();
 route.get("/profile", verifyUser, profile);
+route.post("/update-profile", verifyUser, updateProfile);
 module.exports = route;
